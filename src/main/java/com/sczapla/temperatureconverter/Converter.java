1c5e7315a1768747654d922a2230e710475f0ae5
Sylwia Wójcicka <sylwia.czapla@interia.pl>
2016-07-18 23:07:44 +0200

    przelicza przy wpisaniu wartosci

--- src/main/java/com/sczapla/temperatureconverter/Converter.java
+++ src/main/java/com/sczapla/temperatureconverter/Converter.java
@@ -6,24 +6,27 @@
 package com.sczapla.temperatureconverter;
 
 public class Converter {
-    private final int KELVIN_ZERO = 273;
+    private final double KELVIN_ZERO = 273.15;
+    private final double FAHRENHEIT_ZERO = 459.67;
     
-    public double celciusToFahrenheit(double celcius){
-        return celcius * 1.8 + 32.0;
+    public String celciusToFahrenheit(double celcius){
+        return String.valueOf(celcius * 1.8 + 32.0);
     }
-    public double celciusToKelvin(double celcius){
-        return KELVIN_ZERO+celcius;
+    public String celciusToKelvin(double celcius){
+        return String.valueOf(celcius - KELVIN_ZERO);
     }
-    public double kelvinToCelcius(double kelvin){
-        return 0;
+    public String kelvinToCelcius(double kelvin){
+        return String.valueOf(KELVIN_ZERO+kelvin);
     }
-    public double kelvinToFahrenheit(double kelvin){
-        return 0;
+    public String kelvinToFahrenheit(double kelvin){
+        return String.valueOf((kelvin - KELVIN_ZERO)* 1.8000 + 32.00);
     }
-    public double fahrenheitToCelcius(double fahrenheit){
-        return 0;
+    public String fahrenheitToCelcius(double fahrenheit){
+        return String.valueOf((fahrenheit - 32 ) * 1.8);
     }
-    public double fahrenheitToKelvin(double fahrenheit){
-        return 0;
+    public String fahrenheitToKelvin(double fahrenheit){
+        return  String.valueOf((fahrenheit + FAHRENHEIT_ZERO) * 5/9);
     }
+    
+    
 }
--- src/main/java/com/sczapla/temperatureconverter/FXMLController.java
+++ src/main/java/com/sczapla/temperatureconverter/FXMLController.java
@@ -5,55 +5,67 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 public class FXMLController implements Initializable {
-    
+
     @FXML
     private RadioButton inCelcius;
     @FXML
     private RadioButton inFahrenheit;
     @FXML
     private RadioButton inKelvin;
-    
     @FXML
     private RadioButton outCelcius;
     @FXML
     private RadioButton outFahrenheit;
     @FXML
     private RadioButton outKelvin;
-    
+    @FXML
+    private TextField input;
+    @FXML
+    private TextField output;
+
     private Converter converter;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         inCelcius.setSelected(true);
         outFahrenheit.setSelected(true);
         converter = new Converter();
-    }    
+    }
 
     @FXML
     private void convertValue(ActionEvent event) {
-        
+        if (inCelcius.isSelected()) {
+            celciusConvert();
+        } else if (inFahrenheit.isSelected()) {
+            fahrenheitConvert();
+        } else if (inKelvin.isSelected()) {
+            kelvinConvert();
+        }
     }
 
     @FXML
     private void checkInCelcius(ActionEvent event) {
         inFahrenheit.setSelected(false);
         inKelvin.setSelected(false);
+        celciusConvert();
     }
 
     @FXML
     private void checkInFahrenheit(ActionEvent event) {
         inKelvin.setSelected(false);
         inCelcius.setSelected(false);
+        fahrenheitConvert();
     }
 
     @FXML
     private void checkInKelvin(ActionEvent event) {
         inCelcius.setSelected(false);
         inFahrenheit.setSelected(false);
+        kelvinConvert();
     }
 
     @FXML
@@ -72,5 +84,35 @@ public class FXMLController implements Initializable {
     private void checkOutCelcius(ActionEvent event) {
         outFahrenheit.setSelected(false);
         outKelvin.setSelected(false);
+    }
+
+    private void celciusConvert() {
+        if (outFahrenheit.isSelected()) {
+            output.setText(converter.celciusToFahrenheit(Double.valueOf(input.getText())));
+        } else if (outKelvin.isSelected()) {
+            output.setText(converter.celciusToKelvin(Double.valueOf(input.getText())));
+        } else {
+            output.setText(input.getText());
+        }
+    }
+
+    private void fahrenheitConvert() {
+        if (outCelcius.isSelected()) {
+            output.setText(converter.fahrenheitToCelcius(Double.valueOf(input.getText())));
+        } else if (outKelvin.isSelected()) {
+            output.setText(converter.fahrenheitToKelvin(Double.valueOf(input.getText())));
+        } else {
+            output.setText(input.getText());
+        }
+    }
+
+    private void kelvinConvert() {
+        if (outFahrenheit.isSelected()) {
+            output.setText(converter.kelvinToFahrenheit(Double.valueOf(input.getText())));
+        } else if (outCelcius.isSelected()) {
+            output.setText(converter.kelvinToCelcius(Double.valueOf(input.getText())));
+        } else {
+            output.setText(input.getText());
+        }
     }
 }
